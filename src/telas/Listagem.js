1d9ea828009fc036e0304c75d446e3ce907ff364
JOÃO GABRIEL COSTA FLÔR <21276663@dsn127m1242376.cfp127aluno.local>
2022-09-22 16:50:17 -0300

    Listagem Livros completo

--- src/telas/Listagem.js
+++ src/telas/Listagem.js
@@ -1,62 +1,68 @@
-import React, {useState} from 'react';
-import {Text, View, StyleSheet, ScrollView, SafeAreaView, Image} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  Image,
+} from 'react-native';
 
 import COLORS from '../const/Colors';
 import apiLivraria from '../service/apiLivraria';
-import capaLivro150 from '../assets/livros/lor150.png'
-import capaLivro100 from '../assets/livros/lor100.png'
-import { useEffect } from 'react/cjs/react.production.min';
+import capaLivro150 from '../assets/livros/lor150.png';
+import capaLivro100 from '../assets/livros/lor100.png';
 
 const Listagem = () => {
+  const [livros, setLivros] = useState([]);
 
-    const [livros, setLivros] = useState([])
+  useEffect(() => {
+    apiLivraria.get('/listarLivros').then(data => {
+      console.log(data);
+      setLivros(data.data);
+    });
+  }, []);
 
-    useEffect(()=>{
-        apiLivraria.get('/listarLivros')
-        .then(
-            (data)=>{
-                console.log(data)
-            }
-        )
-    },
-    []
-    )
-
-    return(
+  return (
     <ScrollView>
-        <View style={estilos.container}>
-            <View style={estilos.post}>
-                <Image style={estilos.imagem} source={capaLivro150}/>
-                <Text style={estilos.titulo}>O senhor dos anéis</Text>
-            </View>
-        </View>
+      <View style={estilos.container}>
+        {livros.map(livro => (
+          <View style={estilos.post}>
+            <Image style={estilos.imagem} source={capaLivro150} />
+            <Text style={estilos.titulo}>{livro.titulo}</Text>
+          </View>
+        ))}
+
+        {/* Função com {} retorna um corpo de processamento */}
+        {/* Função quando termina com() retorna um return com tudo que está dentro */}
+      </View>
     </ScrollView>
-    )
-}
+  );
+};
 
 const estilos = StyleSheet.create({
-    container:{
-        alignItems: 'center'
-    },
-    post:{
-        width:'95%',
-        alignItems:'center',
-        backgroundColor:'#ccc',
-        padding: 15,
-        marginVertical: 5,
-        borderRadius:5,
-        elevation:5,
-    },
-    imagem:{
-        borderRadius:5,
-        marginVertical:16,
-        marginLeft:16,
-    },
-    titulo:{
-        textAlign:'center',
-        fontSize:20,
-        fontWeight:'bold',
-    }
-})
+  container: {
+    alignItems: 'center',
+  },
+  post: {
+    width: '95%',
+    alignItems: 'center',
+    backgroundColor: '#ccc',
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 5,
+    elevation: 5,
+  },
+  imagem: {
+    borderRadius: 5,
+    marginVertical: 16,
+    marginLeft: 16,
+  },
+  titulo: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
-export default Listagem
+export default Listagem;
